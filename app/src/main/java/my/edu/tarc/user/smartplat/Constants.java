b9dba22c244e32ed0f36e0c88cb5fe0e0a1c7989
kwekwenwei <kwekww-wa16@student.tarc.edu.my>
2019-01-01 19:22:55 +0800

    123

---
+++ app/src/main/java/my/edu/tarc/user/smartplat/Constants.java
@@ -0,0 +1,6 @@
+package my.edu.tarc.user.smartplat;
+
+public class Constants {
+    public static final String ROOT_URL = "https://tickling-members.000webhostapp.com/";
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
--- app/src/main/java/my/edu/tarc/user/smartplat/RegisterActivity.java
+++ app/src/main/java/my/edu/tarc/user/smartplat/RegisterActivity.java
@@ -2,50 +2,82 @@ package my.edu.tarc.user.smartplat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class RegisterActivity extends AppCompatActivity {
-    private EditText username;
-    private EditText password;
-    private Button register;
-    private SQLiteHandler db;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        register = (Button)findViewById(R.id.register);
+        editTextUsername = (EditText)findViewById(R.id.username);
+        editTextPassword = (EditText)findViewById(R.id.password);
+        buttonRegister = (Button)findViewById(R.id.register);
 
-        //SQLite database handler
-        db = new SQLiteHandler(getApplicationContext());
+        buttonRegister.setOnClickListener(this);
+    }
 
-        register.setOnClickListener(new View.OnClickListener() {
+    private void registerUser(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextUsername.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(),
+                                    jsonObject.getString("message"),
+                                    Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }){
             @Override
-            public void onClick(View v) {
-                String usname = username.getText().toString();
-                String psw = password.getText().toString();
-                if(!usname.isEmpty()&&!psw.isEmpty()){
-                    Toast.makeText(getApplicationContext(),
-                            "Welcome",Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(getApplicationContext(),
-                            "Please Fill In All Detail",Toast.LENGTH_LONG).show();
-                }
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
             }
-        });
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == buttonRegister) {
+            registerUser();
+        }
     }
 }
